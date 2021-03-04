07ac81db9a887c4e051951776fdcc833214f64c7
SzarneckiP <siarniuk@gmail.com>
2021-03-04 14:22:02 +0100

    fix tripListOptions/Container/filtersRedux

--- src/components/features/TripListOptions/TripListOptions.js
+++ src/components/features/TripListOptions/TripListOptions.js
@@ -2,30 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './TripListOptions.scss';
 
-import {Row, Col} from 'react-flexbox-grid';
+import { Row, Col } from 'react-flexbox-grid';
 
 class TripListOptions extends React.Component {
-  handleTags(tag, checked){
-    if(checked) {
+  handleTags(tag, checked) {
+    if (checked) {
       console.log('Adding tag', tag);
       // TODO - use action dispatcher from props
-    } else {
+      this.props.changeFilterTags(tag);
+    } /*else {
       console.log('Removing tag', tag);
       // TODO - use action dispatcher from props
-    }
+    }*/
   }
 
-  handleDuration(type, value){
+  handleDuration(type, value) {
     console.log('Changing duration', type, value);
     // TODO - use action dispatcher from props
+    this.props.changeDuration(value);
   }
 
-  handleSearch(phrase){
+  handleSearch(phrase) {
     this.props.changeSearchPhrase(phrase);
   }
 
-  render(){
-    const {tags, filters} = this.props;
+  render() {
+    const { tags, filters } = this.props;
     return (
       <div className={styles.component}>
         <Row around="lg">
@@ -73,6 +75,8 @@ TripListOptions.propTypes = {
   tags: PropTypes.object,
   filters: PropTypes.object,
   changeSearchPhrase: PropTypes.func,
+  changeFilterTags: PropTypes.func,
+  changeDuration: PropTypes.func,
 };
 
 export default TripListOptions;
--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -1,6 +1,6 @@
 /* SELECTORS */
 
-export const getAllFilters = ({filters}) => filters;
+export const getAllFilters = ({ filters }) => filters;
 
 /* ACTIONS */
 
@@ -11,11 +11,13 @@ const createActionName = name => `app/${reducerName}/${name}`;
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
 // TODO - add other action types
-
+export const CHANGE_TAG = createActionName('CHANGE_TAG');
+export const CHANGE_DURATION = createActionName('CHANGE_DURATION');
 // action creators
 export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
 // TODO - add other action creators
-
+export const changeFilterTags = payload => ({ payload, type: CHANGE_TAG });
+export const changeDuration = payload => ({ payload, type: CHANGE_DURATION });
 // reducer
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
@@ -25,6 +27,16 @@ export default function reducer(statePart = [], action = {}) {
         searchPhrase: action.payload,
       };
     // TODO - handle other action types
+    case CHANGE_TAG:
+      return {
+        ...statePart,
+        tags: action.payload,
+      };
+    case CHANGE_DURATION:
+      return {
+        ...statePart,
+        duration: action.payload,
+      };
     default:
       return statePart;
   }
